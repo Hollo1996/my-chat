f0cb296074f6313839786fbe259d269783433e4c
hollo007 <akos.hollo-szabo@qlmdc.hu>
2020-05-22 18:21:59 +0200

    Release

---
+++ build/precache-manifest.9691eaf41ef6ea25bc2f2dcd3217ee86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d655c7dee2ed182781e41639152a35",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6d98769258ff2319fc32",
+    "url": "./static/css/main.abec8b84.chunk.css"
+  },
+  {
+    "revision": "c05dbb66d7dd9c88eb63",
+    "url": "./static/js/2.3e36a553.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3e36a553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d98769258ff2319fc32",
+    "url": "./static/js/main.59408a56.chunk.js"
+  },
+  {
+    "revision": "763f551d24bf50b1938c",
+    "url": "./static/js/runtime-main.c7b6dbd4.js"
+  }
+]);
